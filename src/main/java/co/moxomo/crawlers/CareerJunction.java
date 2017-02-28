22eb81684a6e0ae3a64a8e493dc9cedb4feea156
Paballo <pdditshego@gmail.com>
2017-02-28 11:00:30 +0200

    Attempting to fix circleci memory issues.

--- src/main/java/co/moxomo/crawlers/CareerJunction.java
+++ src/main/java/co/moxomo/crawlers/CareerJunction.java
@@ -11,105 +11,104 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
-
 /**
  * @author Paballo
- *
  */
 public class CareerJunction {
-	//crawls careerjunction.co.za
-	
-	private static HashSet<String> crawledUrls = new HashSet<String>();
+    //crawls careerjunction.co.za
+
+    private static HashSet<String> crawledUrls = new HashSet<String>();
 
 
-	private static String CAREER = "http://www.careerjunction.co.za";
+    private static String CAREER = "http://www.careerjunction.co.za";
 
-	private static ConcurrentLinkedQueue<String> urlsToCrawl = new ConcurrentLinkedQueue<String>();
+    private static ConcurrentLinkedQueue<String> urlsToCrawl = new ConcurrentLinkedQueue<String>();
 
-	public static void crawl() {
-		CareerJunction c = new CareerJunction();
-		c.crawl(CAREER);
-	}
+    public static void crawl() {
+        CareerJunction c = new CareerJunction();
+        c.crawl(CAREER);
+    }
 
 
-	public void crawl(final String startUrl) {
+    public void crawl(final String startUrl) {
 
-		// Add the start URL to the list of URLs to crawl
-		urlsToCrawl.add(startUrl);
+        // Add the start URL to the list of URLs to crawl
+        urlsToCrawl.add(startUrl);
 
-		// Search until the number of found URLs reaches 2000
-		while (!urlsToCrawl.isEmpty() && crawledUrls.size() < 2000) {
+        // Search until the number of found URLs reaches 2000
+        while (!urlsToCrawl.isEmpty() && crawledUrls.size() < 2000) {
 
-			// Get the URL
-			String url = urlsToCrawl.iterator().next();
+            // Get the URL
+            String url = urlsToCrawl.iterator().next();
 
-			// Remove the URL from the list of URLs to crawl
-			urlsToCrawl.remove(url);
+            // Remove the URL from the list of URLs to crawl
+            urlsToCrawl.remove(url);
 
-			// Verify and convert the URL string to the URL object
-			
+            // Verify and convert the URL string to the URL object
 
-			if (url != null) {
 
-				// Download the page at the URL
-				// String pageContent = fetchPageContent(verifiedUrl);
-				Document doc = null;
-				try {
-					doc = Jsoup.connect(url).timeout(0).get();
-				} catch (Exception e) {
-				}
+            if (url != null) {
 
-				if (doc != null) {
+                // Download the page at the URL
+                // String pageContent = fetchPageContent(verifiedUrl);
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect(url).timeout(0).get();
+                } catch (Exception e) {
+                }
 
-					Elements links = doc.select("a[href]");
+                if (doc != null) {
 
-					for (Element link : links) {
-						String _link = link.attr("abs:href");
-						if (_link.length() < 1) {
-							continue;
-						}
+                    Elements links = doc.select("a[href]");
 
-						int index = _link.indexOf('#');
-						if (index != -1) {
-							_link = _link.substring(0, index);
-						}
+                    for (Element link : links) {
+                        String _link = link.attr("abs:href");
+                        if (_link.length() < 1) {
+                            continue;
+                        }
 
-						if (crawledUrls.contains(_link)) {
-							continue;
-						}
-						if (urlsToCrawl.contains(_link)) {
-							continue;
-						}
-						if (_link.toLowerCase().contains("/my/account/")) {
-							continue;
-							// urlsToCrawl.
-						}
-						if (_link.toLowerCase().contains("-jobs")
-								|| _link.toLowerCase().contains("/jobs/")) {
+                        int index = _link.indexOf('#');
+                        if (index != -1) {
+                            _link = _link.substring(0, index);
+                        }
 
-							urlsToCrawl.add(_link);
-							// urlsToCrawl.
-						}
+                        if (crawledUrls.contains(_link)) {
+                            continue;
+                        }
+                        if (urlsToCrawl.contains(_link)) {
+                            continue;
+                        }
+                        if (_link.toLowerCase().contains("/my/account/")) {
+                            continue;
+                            // urlsToCrawl.
+                        }
+                        if (_link.toLowerCase().contains("-jobs")
+                                || _link.toLowerCase().contains("/jobs/")) {
 
-					}
-					
-					if (url.contains("/jobs/")){
-				   if(url.contains("?searchkey"))	{	
-					String ref = StringUtils.substringBetween(url, "-",
-							"?searchkey").trim();
-					String mobi_url = "http://m.careerjunction.co.za/advert/view.htm?ref="+ref;
-					if (!Util.isAvailable(mobi_url)) {
+                            urlsToCrawl.add(_link);
+                            // urlsToCrawl.
+                        }
 
-					}}
-					
-					}
-				
-					crawledUrls.add(url);
+                    }
 
-				}
-			}
-		}
+                    if (url.contains("/jobs/")) {
+                        if (url.contains("?searchkey")) {
+                            String ref = StringUtils.substringBetween(url, "-",
+                                    "?searchkey").trim();
+                            String mobi_url = "http://m.careerjunction.co.za/advert/view.htm?ref=" + ref;
+                            if (!Util.isAvailable(mobi_url)) {
 
-	}
+                            }
+                        }
+
+                    }
+
+                    crawledUrls.add(url);
+
+                }
+            }
+        }
+
+    }
 
 }
