f4e134afbd660bc145997af300203fe54a0a95e8
Paballo Ditshego <paballo.ditshego@vcontractor.co.za>
2019-04-03 10:57:23 +0200

    WIP alerts

--- src/main/java/za/co/moxomo/config/xmpp/FirebaseUpstreamMessage.java
+++ src/main/java/za/co/moxomo/config/xmpp/FirebaseUpstreamMessage.java
@@ -1,4 +1,64 @@
 package za.co.moxomo.config.xmpp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FirebaseUpstreamMessage {
+	 private String from;
+
+	    private String category;
+
+	    private Map<String, Object> data = new HashMap<>();
+
+	    private String to;
+
+	    @JsonProperty("message_id")
+	    private String messageId;
+
+
+	    @JsonProperty("message_type")
+	    private String messageType;
+
+	
+	    public String getFrom() {
+	        return from;
+	    }
+
+	    public String getCategory() {
+	        return category;
+	    }
+
+	    public Map<String, Object> getData() {
+	        return data;
+	    }
+
+	
+	    public String getTo() {
+	        return to;
+	    }
+
+	 
+	    public String getMessageUid() {
+	        return messageId;
+	    }
+
+
+	    public String getMessageType() {
+	        return messageType;
+	    }
+
+	
+	    public String toString() {
+	        return "GcmUpstreamMessage{" +
+	                "messageType='" + messageType + '\'' +
+	                ", from=" + from +
+	                ", to=" + to +
+	                ", category=" + category +
+	                ", data=" + data +
+	                '}';
+	    }
 }
--- src/main/java/za/co/moxomo/config/xmpp/InboundFcmMessageHandler.java
+++ src/main/java/za/co/moxomo/config/xmpp/InboundFcmMessageHandler.java
@@ -1,5 +1,107 @@
-package za.co.moxomo.config.xmpp;
 
+import org.jivesoftware.smack.packet.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
 public class InboundFcmMessageHandler {
 
+
+    @Autowired
+    private MessageChannel fcmXmppOutboundChannel;
+
+
+
+    private static final String ORIGINAL_MESSAGE_ID = "original_message_id";
+
+    @ServiceActivator(inputChannel = "gcmInboundChannel")
+    public void handleUpstreamMessage(FirebaseUpstreamMessage message) throws Exception {
+        String message_type = message.getMessageType();
+        if (message_type == null) {
+            handleOrdinaryMessage(message);
+        } else {
+            switch (message_type) {
+                case "ack":
+                    handleAcknowledgementReceipt(message);
+                    break;
+                case "nack":
+                    handleNotAcknowledged(message);
+                    break;
+                case "receipt":
+                    handleDeliveryReceipts(message);
+                    break;
+                case "control":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    private void handleAcknowledgementReceipt(FirebaseUpstreamMessage input) {
+        String messageId = input.getMessageUid();
+        String data = String.valueOf(input.getData());
+        log.debug("Gcm acknowledges receipt of message {}, with payload {}", messageId, data);
+
+    }
+
+    private void handleOrdinaryMessage(FirebaseUpstreamMessage input) {
+        log.debug("Ordinary message received");
+        String messageId = input.getMessageUid();
+        String from = input.getFrom();
+
+        String action = String.valueOf(input.getData().get("action"));
+   /*     if (action != null) {
+            switch (action) {
+                case "REGISTER":
+                    String phoneNumber = (String) input.getData().get("phoneNumber");
+                    registerUser(from, phoneNumber);
+                    break;
+                case "UPDATE_READ":
+                    String notificationId = (String) input.getData().get("notificationId");
+                    updateReadStatus(notificationId);
+                    break;
+                default: //action unknown ignore
+                    break;
+
+            }
+        }*/
+        sendAcknowledment(from, messageId);
+    }
+
+    private void handleNotAcknowledged(FirebaseUpstreamMessage input) {
+        String messageId = input.getMessageUid();
+      /*  Notification notification = notificationService.loadNotification(messageId);
+        if (notification != null) {
+            log.info("Push Notification delivery failed, now sending SMS to  {}", notification.getTarget().getPhoneNumber());
+            messageSendingService.sendMessage(UserMessagingPreference.SMS.name(), notification);
+        } else {
+            log.info("Received an upstream message without notification, must be chat message");
+        }*/
+    }
+
+    private void handleDeliveryReceipts(FirebaseUpstreamMessage input) {
+        String messageId = String.valueOf(input.getData().get(ORIGINAL_MESSAGE_ID));
+        log.debug("Message " + messageId + " delivery successful, updating notification to delivered status.");
+       // notificationService.markNotificationAsDelivered(messageId);
+    }
+
+
+    private void sendAcknowledment(String registrationId, String messageId) {
+        org.springframework.messaging.Message<Message> gcmMessage = FirebaseXmppMessageCodec.encode(registrationId, messageId, "ack");
+        log.debug("Acknowledging message with id ={}", messageId);
+        fcmXmppOutboundChannel.send(gcmMessage);
+    }
+
+   
+
+  
 }
